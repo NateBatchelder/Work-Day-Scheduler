19c6f5462cdc062106c6d9bcf60aa1f1f856350b
Nathan Batchelder <natebatchelder@gmail.com>
2021-07-06 18:30:48 -0700

    added event listner for button click & added color coding classifications with JSON

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,37 @@
-$("#date-my").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+$(document).ready(function(){
+    $("#date-my").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+})
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function (){
+        var text =(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })
+
+    function timeTracker() {
+        var timeNow = moment().hour(); //uses moment to set a variable of 'timeNow'
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+            // changes color of background 
+            if (blockTime < timeNow) { // first conditional - if the event is in the past
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) { // second conditional - if the event is now
+                $(this).removeClass("future");
+                $(this).addClass("present");
+                $(this).removeClass("past");
+            }
+            else { // last conditional - if the event is in the future
+                $(this).addClass("future");
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+            }
+        })
+    }
+
+
+
+})
